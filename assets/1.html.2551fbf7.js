a6c94716cc64b3cc04210bcc3060974dacc3f6fa
Fankozhang <75946389+Fankozhang@users.noreply.github.com>
2023-01-19 14:02:05 +0000

    Deploying to gh-pages from @ Fankozhang/blog@c4e028e56d92324605b814e1b5a56ee58c3d7c7e 🚀

---
+++ assets/1.html.2551fbf7.js
@@ -0,0 +1,92 @@
+import{_ as r}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as d,c as v,a as n,b as e,e as a,f as t,r as l}from"./app.7fbc303d.js";const o="/blog/javaCreateSimple.png",c="/blog/javaWebCreate.png",m="/blog/javaWebCreate2.png",p="/blog/javaWebCreate3.png",u="/blog/javaWebCreate4.png",g="/blog/javaModuleCreate.png",b="/blog/javaModuleCreate2.png",h="/blog/javaModuleCreate3.png",_="/blog/javaModuleCreate4.png",j="/blog/javaModuleCreate5.png",x="/blog/javaModuleCreate6.png",f="/blog/javaModuleCreate7.png",S="/blog/javaModuleCreate8.png",y={},C=t('<h1 id="java" tabindex="-1"><a class="header-anchor" href="#java" aria-hidden="true">#</a> Java</h1><h2 id="idea创建java项目-maven" tabindex="-1"><a class="header-anchor" href="#idea创建java项目-maven" aria-hidden="true">#</a> <strong>idea创建java项目（maven）</strong></h2><h3 id="_1-创建普通java项目-quickstart" tabindex="-1"><a class="header-anchor" href="#_1-创建普通java项目-quickstart" aria-hidden="true">#</a> <strong>1 创建普通Java项目（quickstart）</strong></h3><p><img src="'+o+'" alt="javaCreateSimple" loading="lazy"></p><h3 id="_2创建普通java-web项目-maven-archetype-webapp" tabindex="-1"><a class="header-anchor" href="#_2创建普通java-web项目-maven-archetype-webapp" aria-hidden="true">#</a> 2创建普通Java web项目（maven-archetype-webapp）</h3><p><img src="'+c+`" alt="javaWebCreate" loading="lazy"></p><p>pom.xml</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&lt;properties&gt;
+  &lt;project.build.sourceEncoding&gt;UTF-8&lt;/project.build.sourceEncoding&gt;
+  &lt;maven.compiler.source&gt;1.8&lt;/maven.compiler.source&gt;
+  &lt;maven.compiler.target&gt;1.8&lt;/maven.compiler.target&gt;
+&lt;/properties&gt;
+
+&lt;dependencies&gt;
+  &lt;dependency&gt;
+    &lt;groupId&gt;junit&lt;/groupId&gt;
+    &lt;artifactId&gt;junit&lt;/artifactId&gt;
+    &lt;version&gt;4.12&lt;/version&gt;
+    &lt;scope&gt;test&lt;/scope&gt;
+  &lt;/dependency&gt;
+&lt;/dependencies&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,8),M=t(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&lt;build&gt;
+  &lt;finalName&gt;simple-java-web&lt;/finalName&gt;
+  &lt;plugins&gt;
+    &lt;plugin&gt;
+
+      &lt;groupId&gt;org.eclipse.jetty&lt;/groupId&gt;
+
+      &lt;artifactId&gt;jetty-maven-plugin&lt;/artifactId&gt;
+
+      &lt;version&gt;9.4.11.v20180605&lt;/version&gt;
+
+      &lt;configuration&gt;
+
+        &lt;scanIntervalSeconds&gt;10&lt;/scanIntervalSeconds&gt;
+
+        &lt;webAppConfig&gt;
+
+          &lt;contextPath&gt;/test&lt;/contextPath&gt;
+
+        &lt;/webAppConfig&gt;
+
+      &lt;/configuration&gt;
+
+    &lt;/plugin&gt;
+
+    &lt;plugin&gt;
+      &lt;groupId&gt;org.apache.tomcat.maven&lt;/groupId&gt;
+      &lt;artifactId&gt;tomcat7-maven-plugin&lt;/artifactId&gt;
+      &lt;version&gt;2.2&lt;/version&gt;
+      &lt;configuration&gt;
+        &lt;port&gt;8081&lt;/port&gt; &lt;!--启动的端口号，默认8081 --&gt;
+        &lt;path&gt;/test&lt;/path&gt; &lt;!-- 项目站点名，对外访问路径 --&gt;
+        &lt;uriEncoding&gt;UTF-8&lt;/uriEncoding&gt;
+        &lt;server&gt;tomcat7&lt;/server&gt;
+      &lt;/configuration&gt;
+    &lt;/plugin&gt;
+  &lt;/plugins&gt;
+&lt;/build&gt;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p><img src="`+m+'" alt="javaWebCreate2" loading="lazy"><img src="'+p+'" alt="javaWebCreate3" loading="lazy"><img src="'+u+'" alt="javaWebCreate4" loading="lazy"></p>',2),q={href:"http://localhost:8080/test",target:"_blank",rel:"noopener noreferrer"},k={href:"http://localhost:8081/test",target:"_blank",rel:"noopener noreferrer"},I=t('<h3 id="_3创建多模块项目" tabindex="-1"><a class="header-anchor" href="#_3创建多模块项目" aria-hidden="true">#</a> <strong>3创建多模块项目</strong></h3><p>创建项目 项目名 maven-parent</p><p><img src="'+g+'" alt="javaModuleCreate" loading="lazy"></p><p>创建 module 3个</p><p><img src="'+b+'" alt="javaModuleCreate2" loading="lazy"></p><p><img src="'+h+'" alt="javaModuleCreate3" loading="lazy"></p><p>maven-dao maven-service maven-controller（3个模块） 创建maven-dao maven-service <strong>用quickstart</strong> 创建maven-controller <strong>用maven-archetype-webapp</strong></p><p><strong>生成目录结构</strong></p><p><img src="'+_+'" alt="javaModuleCreate4" loading="lazy"></p><p>修改所有的子模块的pom.xml配置</p><p><img src="'+j+`" alt="javaModuleCreate5" loading="lazy"></p><p>UserDao</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>package org.example.dao;
+
+public class UserDao {
+    public static void testDao(){
+        System.out.println(&quot;testDao&quot;);
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>UserService</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>package org.example.service;
+
+import org.example.dao.UserDao;
+
+public class UserService {
+    public static void testService() {
+        System.out.println(&quot;userService&quot;);
+        // 调用maven-dao模块的方法
+        UserDao.testDao();
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>UserServlet</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>package org.example;
+
+import org.example.service.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(&quot;/user&quot;)
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println(&quot;userServlet&quot;);
+        UserService.testService();
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>在三个module的pom.xml引入相关联模块的依赖</p><p>dao不用引</p><p><img src="`+x+'" alt="javaModuleCreate6" loading="lazy"></p><p><img src="'+f+'" alt="javaModuleCreate7" loading="lazy"></p><p><img src="'+S+'" alt="javaModuleCreate8" loading="lazy"></p><p>配置maven命令，启动</p><h2 id="java-ssm框架" tabindex="-1"><a class="header-anchor" href="#java-ssm框架" aria-hidden="true">#</a> java SSM框架</h2>',24),w={href:"https://www.bilibili.com/video/BV1Ya411S7aT/?spm_id_from=333.337.search-card.all.click&vd_source=f25f5a8d75a3a60d5a288f726803ec11",target:"_blank",rel:"noopener noreferrer"},z={href:"https://pan.baidu.com/s/1jx1hRb7ugMFFJP81Fvcy1Q?pwd=yyds",target:"_blank",rel:"noopener noreferrer"},U={href:"https://gitee.com/zhengguangqq/ssm-md",target:"_blank",rel:"noopener noreferrer"};function E(W,F){const s=l("pluginManagement"),i=l("ExternalLinkIcon");return d(),v("div",null,[C,n("p",null,[e("删除pom.xml中 "),a(s),e(" 标签和里面的内容 plugins标签中加了jetty和tomcat,可以测试项目运行")]),M,n("p",null,[e("( compile命令编译 package命令打包 ) 访问路径为 ： jetty "),n("a",q,[e("http://localhost:8080/test"),a(i)]),e(" tomcat "),n("a",k,[e("http://localhost:8081/test"),a(i)])]),I,n("p",null,[e("决定看ssm框架视频， "),n("a",w,[e("【尚硅谷】SSM框架全套教程，MyBatis+Spring+SpringMVC+SSM整合一套通关_哔哩哔哩_bilibili"),a(i)]),e(" 。")]),n("p",null,[e("需要资料当然要到评论区取找了，资源链接："),n("a",z,[e("https://pan.baidu.com/s/1jx1hRb7ugMFFJP81Fvcy1Q?pwd=yyds"),a(i)]),e(" 提取码：yyds 。 "),n("a",U,[e("https://gitee.com/zhengguangqq/ssm-md"),a(i)]),e(" 尚硅谷的ssm md格式笔记")])])}const N=r(y,[["render",E],["__file","1.html.vue"]]);export{N as default};
---
+++ assets/bigScreen.html.fd551341.js
@@ -0,0 +1,58 @@
+import{_ as r}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as d,c as t,a as e,b as n,e as s,f as a,r as l}from"./app.7fbc303d.js";const c={},v=e("h1",{id:"bigscreen",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#bigscreen","aria-hidden":"true"},"#"),n(" bigScreen")],-1),o=e("h2",{id:"echarts",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#echarts","aria-hidden":"true"},"#"),n(" echarts")],-1),u={href:"https://blog.csdn.net/luanpeng825485697/category_11735465.html",target:"_blank",rel:"noopener noreferrer"},m=e("h3",{id:"echarts设置主题",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#echarts设置主题","aria-hidden":"true"},"#"),n(" echarts设置主题")],-1),h={href:"https://echarts.apache.org/zh/theme-builder.html",target:"_blank",rel:"noopener noreferrer"},b=a(`<p>在主题中复制 js 代码并保存到 js 文件中，引入到需要的页面，如保存到 theme.js（<strong>记住主题名称，如‘vintage’</strong>）（‘dark’主题是内置的，无需引入）</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>import &quot;../assets/theme&quot;    // 引入js文件
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>使用时，获取dom时填入第二个参数（<strong>主题名称，切记不是js文件名称</strong>）</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>var myChart2 = echarts.init(this.$refs.map2,&#39;vintage&#39;)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>随后绘制的图表就会用上设置的主题样式了。</p><h3 id="vue使用echarts渲染地图数据" tabindex="-1"><a class="header-anchor" href="#vue使用echarts渲染地图数据" aria-hidden="true">#</a> vue使用echarts渲染地图数据</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>npm install echarts --save
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>在使用echarts的页面引入</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>import * as echarts from &#39;echarts&#39;;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div>`,9),p={href:"https://datav.aliyun.com/portal/school/atlas/area_selector#&lat=31.84139930209406&lng=117.33123779296875&zoom=9",target:"_blank",rel:"noopener noreferrer"},g=a(`<div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>import JSON from &#39;../assets/jinan.json&#39;
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><p>methods里编写获取dom元素容器，渲染数据的方法，并在mounted调用</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>&lt;div ref=&quot;map&quot; id=&quot;main&quot; style=&quot;width:500x;height:500px&quot;&gt;
+
+mounted(){
+    this.drawMap()
+  },
+  methods:{
+    drawMap(){
+        var myChart = echarts.init(this.$refs.map) // 拿到一个实例
+        // var myChart = echarts.init(document.getElementById(&quot;main&quot;)) //也可以用id获取实例
+          echarts.registerMap(&#39;济南市&#39;, JSON, {}) //引入地图文件
+          var option = {
+            series: [
+              {
+                type: &#39;map&#39;,
+                mapType: &#39;济南市&#39;//地图名称，要和引入地图文件的第一个参数名相同
+
+              }]
+          }
+          myChart.setOption(option)
+          
+           // 地图点击每一块区域事件（根据点击事件参数，进行逻辑处理）
+          myChart.on(&#39;click&#39;, function (params) {
+            console.log(params)
+            //逻辑控制
+          })
+          
+           // resize方法监听图表容器的大小并改变图表大小
+           window.onresize = function() {
+                myChart.resize();
+              }
+          
+      }
+
+  }
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>对于配置series绘制地图，还可以通过配置geo来设置地图，如下：</p><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>var option={
+	geo:{
+      type:&quot;map&quot;,
+      map:&quot;china&quot;,//使用 registerMap 注册的地图名称
+      // 默认设置完地图是固定死的不能拖动
+      roam:true,//否开启鼠标缩放和平移漫游。默认不开启。
+      zoom :1,//当前视角的缩放比例。越大比例越大
+      center:[108.956239,34.268309],
+        label:{//地图上显示文字提示信息
+        show:true,
+        color:&quot;#ff6600&quot;,
+        fontSize:10//字体大小
+        },
+        itemStyle:{//地图区域的多边形 图形样式。
+          areaColor:&quot;#f8bf1c&quot;//地图区域的颜色。
+        }
+     },
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5);function x(_,f){const i=l("ExternalLinkIcon");return d(),t("div",null,[v,o,e("p",null,[n("对于echarts图标的配置可以参考 "),e("a",u,[n("Echarts数据可视化_腾讯数据架构师的博客-CSDN博客"),s(i)])]),m,e("p",null,[e("a",h,[n("主题编辑器 - Apache ECharts"),s(i)])]),b,e("p",null,[n("引入地图的 json 数据 "),e("a",p,[n("DataV.GeoAtlas地理小工具系列 (aliyun.com)"),s(i)])]),g])}const C=r(c,[["render",x],["__file","bigScreen.html.vue"]]);export{C as default};
